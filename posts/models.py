2cf13348f91c3ccdd62ab7783c3be1fc167c5ec1
Rndmkid <tonny.huang0724@gmail.com>
2018-11-22 15:16:56 -0500

    Merged Comment/Post Model

--- posts/models.py
+++ posts/models.py
@@ -9,24 +9,17 @@ def Post(models.Model):
     subreddit = models.ForeignKey('subreddit.Sub', on_delete=models.DO_NOTHING)
     author = models.ForeignKey('accounts.User', on_delete=models.DO_NOTHING)
     votes = models.IntegerField(default=0)
-    vote = GenericRelation(Vote)
-    
+    response_to = models.ForeignKey('posts.Post',
+                                    on_delete=models.DO_NOTHING,
+                                    null=True,
+                                    blank=True)
+    isComment = models.BooleanField(default=False)
+    content = models.TextField()
+    title = models.CharField(max_length=50)
 
 def Vote(models.Model):
     value = models.IntegerField()
     owner = models.ForeignKey('accounts.User', on_delete=models.DO_NOTHING)
-##    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = fields.GenericForeignKey('content_type', 'object_id')
-
-def Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     
-    vote = GenericRelation(Vote)
-    
-
-####
-##Consider Merging Post/Comment
-####
